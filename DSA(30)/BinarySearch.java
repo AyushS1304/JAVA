29bf70ea92c332cf4babcbc0a2fdb5905c5655b9
AyushS1304 <ayushsharma130408@gmail.com>
2026-07-21 16:27:50 +0530

    Binary Search

--- DSA(30)/BinarySearch.java
+++ DSA(30)/BinarySearch.java
@@ -1,24 +1,34 @@
 public class BinarySearch {
     public static void Search(int []arr,int x){
-        int n=arr.length;
+        int left=0;
+        int right=arr.length-1;
         boolean found=false;
-        for(int i=0;i<n;i++){
-            if(arr[i]==x){
+
+        while(left<=right){
+            int mid=(left+right)/2;
+
+            if(arr[mid]==x){
                 found=true;
                 break;
-            }           
+            }
+            else if(arr[mid]<x){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
         }
+
         if(!found){
             System.out.println("NO SUCH ELEMENT FOUND!!!");
         }
         if(found==true){
             System.out.println("Element Found!!!");
         }
-        
     }
+
     public static void main(String[] args) {
         int []arr={1,2,3,4,5};
         Search(arr, 5);
-
     }
 }
